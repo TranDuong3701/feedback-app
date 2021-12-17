3e1f9836c4f44c4487ecd9f9d4c5cef8cf9ad201
TranDuong3701 <tranduong3701@gmail.com>
2021-12-17 15:16:44 +0700

    finished app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import Header from "./components/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { FeedbackProvider } from "./context/FeedbackContext";
-import Homepage from "./components/pages/Homepage";
+import Homepage from "./pages/Homepage";
+import AboutIconLink from "./components/AboutIconLink";
+import AboutPage from "./pages/AboutPage";
+
 function App() {
-    return (
-        <FeedbackProvider>
-            <Router>
-                <Header />
-                <div className="container">
-                    <Routes>
-                        <Route exact path="/" element={<Homepage />} />
-                    </Routes>
-                </div>
-            </Router>
-        </FeedbackProvider>
-    );
+  return (
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Homepage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
+  );
 }
 
 export default App;
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,59 +1,80 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
 import RatingSelect from "./RatingSelect";
 import FeedbackContext from "../context/FeedbackContext";
 
 const FeedbackForm = () => {
-    const [text, setText] = useState("");
-    const [message, setMessage] = useState("");
-    const [rating, setRating] = useState(10);
-    const [isDisabled, setIsDisabled] = useState(true);
-    const [isEditMode, setIsEditMode] = useState(true);
+  const [text, setText] = useState("");
+  const [message, setMessage] = useState("");
+  const [rating, setRating] = useState(10);
+  const [isDisabled, setIsDisabled] = useState(true);
+  const { addFeedback, updateFeedback, feedbackEdit, editFeedback } =
+    useContext(FeedbackContext);
 
-    const { addFeedback } = useContext(FeedbackContext);
+  const handleTextChange = (e) => {
+    if (!text) {
+      setMessage(null);
+      setIsDisabled(false);
+    } else if (text && text.trim().length <= 10) {
+      setMessage("Text must be at least 10 characters");
+      setIsDisabled(true);
+    } else {
+      setMessage(null);
+      setIsDisabled(false);
+    }
 
-    const handleTextChange = (e) => {
-        if (!text) {
-            setMessage(null);
-            setIsDisabled(false);
-        } else if (text && text.trim().length <= 15) {
-            setMessage("Text must be at least 10 characters");
-            setIsDisabled(true);
-        } else {
-            setMessage(null);
-            setIsDisabled(false);
-        }
+    setText(e.target.value);
+  };
 
-        setText(e.target.value);
-    };
+  useEffect(() => {
+    if (feedbackEdit.edit === true) {
+      setIsDisabled(false);
+      setText(feedbackEdit.feedback.text);
+      setRating(feedbackEdit.feedback.rating);
+    }
+  }, [feedbackEdit]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        addFeedback({ text, rating });
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      };
 
-    return (
-        <Card>
-            <form onSubmit={handleSubmit}>
-                <h2>How would you rate your service with us?</h2>
-                <RatingSelect select={(rating) => setRating(rating)} />
-                <div className="input-group">
-                    <input
-                        type="text"
-                        placeholder="Write a review"
-                        onChange={handleTextChange}
-                        value={text}
-                    />
-                    <Button type="submit" isDisabled={isDisabled}>
-                        Send
-                    </Button>
-                </div>
+      if (feedbackEdit.edit) {
+        updateFeedback(feedbackEdit.feedback.id, newFeedback);
+        editFeedback(feedbackEdit.feedback, false);
+      } else {
+        addFeedback(newFeedback);
+      }
+      setRating(10);
+      setText("");
+    }
+  };
 
-                {message && <small className="message">{message}</small>}
-            </form>
-        </Card>
-    );
+  return (
+    <Card>
+      <form onSubmit={handleSubmit}>
+        <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="Write a review"
+            onChange={handleTextChange}
+            value={text}
+          />
+          <Button type="submit" isDisabled={isDisabled}>
+            Send
+          </Button>
+        </div>
+
+        {message && <small className="message">{message}</small>}
+      </form>
+    </Card>
+  );
 };
 
 export default FeedbackForm;
