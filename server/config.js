992602ab86a0adecc8f99ae8d81e211011c89e13
Trần Khắc Bình Dương <tranduong3701@gmail.com>
2022-01-16 17:27:28 +0700

    updated .env server

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 exports.connect = async () => {
     try {
-        await mongoose.connect("mongodb://localhost/feedbacks", {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
         });
         console.log("Connect db successfully!");
